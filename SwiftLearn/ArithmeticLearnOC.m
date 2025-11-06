3de4c89f58b9640aeac236b4618db6124d5857c9
weikeyan <weikeyan@drojian.dev>
2025-11-06 17:40:34 +0800

    update:0 移动到数组的末尾

--- SwiftLearn/ArithmeticLearnOC.m
+++ SwiftLearn/ArithmeticLearnOC.m
@@ -581,4 +581,20 @@
     
     return longgestNum;
 }
+
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+//请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+- (void)moveZeroes:(NSMutableArray *)nums {
+    NSInteger i = 0;
+    for (NSInteger j = 0; j < nums.count; j++) {
+        if ([nums[j] integerValue] != 0) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    for (NSInteger j = i; j < nums.count; j++) {
+        nums[j] = @(0);
+    }
+}
 @end
